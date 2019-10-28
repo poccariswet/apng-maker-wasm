9e2cdf6887c869a08d45d7db1f30cd1b19beace5
poccariswet <s1240231@gmail.com>
2019-10-28 16:34:12 +0900

    [WIP] apng_encode err...

--- front/index.js
+++ front/index.js
@@ -4,7 +4,12 @@ document.getElementById('file_input').onchange = function() {
   let files = document.getElementById('file_input').files;
   console.log(files);
 
-  wasm.apng_encode_all();
+  var fileReader = new FileReader();
+  fileReader.onload = function () {
+    console.log( fileReader.result ) ; // ArrayBuffer
+    wasm.apngEncode(new Uint8Array(fileReader.result));
+  }
 
-  wasm.using_macro();
+  // 読み込みを実行
+  fileReader.readAsArrayBuffer(files[0]);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 mod utils;
 
+use apng;
+use apng::Encoder;
+use apng::{Frame, PNGImage};
+use std::io::BufWriter;
+
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -18,7 +23,28 @@ macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
-#[wasm_bindgen]
-pub fn apng_encode_all(files: Vec<&str>) {
-    console_log!("{}", files[0])
+#[wasm_bindgen (js_name = apngEncode)]
+pub fn apng_encode(data: Vec<u8>) {
+    let img = image::load_from_memory_with_format(&data, image::ImageFormat::PNG).unwrap();
+
+    let mut png_images: Vec<PNGImage> = Vec::new();
+    png_images.push(apng::load_dynamic_image(img).unwrap());
+
+    let mut buf_writer = BufWriter::new(Vec::new());
+
+    let config = apng::create_config(&png_images, None).unwrap();
+    let mut encoder = Encoder::new(&mut buf_writer, config).unwrap();
+    let frame = Frame {
+        delay_num: Some(1),
+        delay_den: Some(2),
+        ..Default::default()
+    };
+
+    match encoder.encode_all(png_images, Some(&frame)) {
+        Ok(_n) => log("success"),
+        Err(err) => console_log!("{}", err),
+    }
+
+    let buffer = buf_writer.is_empty();
+    console_log!("{}", buffer);
 }
